00a2ceae6020e0350e0eb94b60026151455ee56b
alexgpeppe <alexgpeppe84@hotmail.it>
2020-04-08 10:44:43 +0200

    [#172145511] Add endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import * as bodyParser from "body-parser";
 import * as express from "express";
+import { Errors } from "io-ts";
+import { errorsToReadableMessages } from "italia-ts-commons/lib/reporters";
+import { CreateOrOverwriteInstallationBody } from "./generated/definitions/CreateOrOverwriteInstallationBody";
+import { SendTemplateNotificationBody } from "./generated/definitions/SendTemplateNotificationBody";
+import { log } from "./utils/logger";
 
 // Create Express server
 const app = express();
@@ -10,7 +15,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (_0, res) => {
-  res.json({ text: "Hello world!" });
+  res.json({ message: "The server is up and running" });
 });
+
+app.post(
+  "/:notificationHub/messages",
+  bodyParser.json({ limit: "64Kb" }),
+  (req, res) =>
+    res
+      .status(
+        SendTemplateNotificationBody.decode(req.body).fold(
+          invalidPayloadHandler,
+          () => 200
+        )
+      )
+      .end()
+);
+
+app.put("/:notificationHub/installations", (req, res) =>
+  res
+    .status(
+      CreateOrOverwriteInstallationBody.decode(req.body).fold(
+        invalidPayloadHandler,
+        () => 204
+      )
+    )
+    .end()
+);
+
+app.delete("/:notificationHub/installations/:installationId", (_, res) =>
+  res.status(204).end()
+);
+
+function invalidPayloadHandler(errors: Errors): number {
+  log.info("Invalid payload. ", errorsToReadableMessages(errors).join(" / "));
+  return 400;
+}
 
 export default app;
